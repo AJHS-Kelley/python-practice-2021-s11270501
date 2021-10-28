da46e35c3424a5768e23c9d57fa52ccd9bb5853a
Joshua V <70594931+s11270501@users.noreply.github.com>
2021-10-28 12:43:48 -0400

    Create variables_practice.py

---
+++ variables/variables_practice.py
@@ -0,0 +1,11 @@
+# Variable Practice, Ryan Kelley,10/28/21 11:39 am,version 0.0
+
+#Variable Types -- Most Common
+
+my_float = 2.1
+my_int =69
+my_string = "I like trains"
+
+print(my_float)
+print(my_int)
+print(my_string)
